63dd32298ad15e96ca49547e5be57714ec537993
taylus <brandon@legacy.codes>
2020-01-10 23:40:46 -0600

    Implement ROM loading and memory bus (#6, #7)

---
+++ GBDotNet.Core.Test/Integration/MemoryBus_Should.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GBDotNet.Core.Test.Integration
+{
+    [TestClass]
+    public class MemoryBus_Should
+    {
+        private const byte marker = 0xFF;
+
+        [TestMethod]
+        public void Expose_Rom_Bank_Zero_From_0x0000_To_0x3FFF()
+        {
+            var romData = Enumerable.Repeat(marker, RomFile.BankSize);
+            var rom = new RomFile(romData.ToArray());
+            var memoryBus = new MemoryBus();
+
+            memoryBus.LoadRom(rom);
+
+            for(int i = 0; i < RomFile.BankSize; i++)
+            {
+                Assert.AreEqual(marker, memoryBus[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Expose_Rom_Bank_One_From_0x4000_To_0x7FFF()
+        {
+            var romData = Enumerable.Repeat(marker, RomFile.BankSize * 2);
+            var rom = new RomFile(romData.ToArray());
+            var memoryBus = new MemoryBus();
+
+            memoryBus.LoadRom(rom);
+
+            for (int i = RomFile.BankSize; i < RomFile.BankSize * 2; i++)
+            {
+                Assert.AreEqual(marker, memoryBus[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Expose_Read_Writable_Work_Ram_From_0xC000_To_0xDFFF()
+        {
+            var memoryBus = new MemoryBus();
+
+            for (int i = 0xC000; i <= 0xDFFF; i++)
+            {
+                memoryBus[i] = marker;
+            }
+
+            for (int i = 0xC000; i <= 0xDFFF; i++)
+            {
+                Assert.AreEqual(marker, memoryBus[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Expose_Echo_Ram_From_0xE000_To_0xFDFF()
+        {
+            var memoryBus = new MemoryBus();
+
+            //values written to work RAM at $C000 - $DFFF are repeated in the "echo RAM" range
+            for (int i = 0xC000; i <= 0xDFFF; i++)
+            {
+                memoryBus[i] = marker;
+            }
+
+            for (int i = 0xE000; i <= 0xFDFF; i++)
+            {
+                Assert.AreEqual(marker, memoryBus[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Expose_Read_Writable_Zero_Page_From_0xFF80_To_0xFFFE()
+        {
+            var memoryBus = new MemoryBus();
+
+            for (int i = 0xFF80; i < 0xFFFF; i++)
+            {
+                memoryBus[i] = marker;
+            }
+
+            for (int i = 0xFF80; i < 0xFFFF; i++)
+            {
+                Assert.AreEqual(marker, memoryBus[i]);
+            }
+        }
+    }
+}
---
+++ GBDotNet.Core/Memory/MemoryBus.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GBDotNet.Core
+{
+    /// <summary>
+    /// Implements the Game Boy's address space, routing addresses to
+    /// RAM, ROM, or I/O devices according to the Game Boy's memory map.
+    /// </summary>
+    /// <see cref="http://gameboy.mongenel.com/dmg/asmmemmap.html"/>
+    public class MemoryBus : IMemory
+    {
+        private RomFile rom;
+        private Memory wram = new Memory();
+        private Memory zram = new Memory();
+        private byte interruptEnableFlag = 0;
+
+        public void LoadRom(RomFile rom)
+        {
+            this.rom = rom;
+        }
+
+        public byte this[int index]
+        {
+            get
+            {
+                if (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Address cannot be negative.");
+                }
+                if (index < 0x4000)
+                {
+                    return rom[index];  //ROM bank 0 (16K)
+                }
+                else if (index < 0x8000)
+                {
+                    return rom[index];  //ROM bank 1 (16K) -- TODO: bank switching
+                }
+                else if (index < 0xA000)
+                {
+                    //VRAM (8K)
+                    throw new NotImplementedException($"Unsupported read of address ${index:X4}: Video RAM is not yet implemented.");
+                }
+                else if (index < 0xC000)
+                {
+                    //external RAM (8K)
+                    throw new NotImplementedException($"Unsupported read of address ${index:X4}: Cartridge RAM is not yet implemented.");
+                }
+                else if (index < 0xE000)
+                {
+                    //internal work RAM (8K)
+                    return wram[index - 0xC000];
+                }
+                else if (index < 0xFE00)
+                {
+                    //internal work RAM shadow
+                    return wram[index - 0xE000];
+                }
+                else if (index < 0xFEA0)
+                {
+                    //sprite OAM (160 bytes)
+                    throw new NotImplementedException($"Unsupported read of address ${index:X4}: Sprite OAM is not yet implemented.");
+                }
+                else if (index < 0xFF00)
+                {
+                    //unusable memory range, should always read as zero
+                    return 0;
+                }
+                else if (index < 0xFF80)
+                {
+                    //various hardware I/O registers (PPU, APU, joypad, etc)
+                    throw new NotImplementedException($"Unsupported read of address ${index:X4}: Hardware I/O registers are not yet implemented.");
+                }
+                else if (index < 0xFFFF)
+                {
+                    //zero page (128 bytes)
+                    return zram[index - 0xFF80];
+                }
+                else if (index == 0xFFFF)
+                {
+                    return interruptEnableFlag;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Address must be between $0000 and $FFFF");
+                }
+            }
+
+            set
+            {
+                if (index >= 0x8000 && index < 0xA000)
+                {
+                    //VRAM (8K)
+                    throw new NotImplementedException($"Unsupported write to address ${index:X4}: Video RAM is not yet implemented.");
+                }
+                else if (index < 0xC000)
+                {
+                    //external RAM (8K)
+                    throw new NotImplementedException($"Unsupported write to address ${index:X4}: Cartridge RAM is not yet implemented.");
+                }
+                else if (index < 0xE000)
+                {
+                    //internal work RAM (8K)
+                    wram[index - 0xC000] = value;
+                }
+                else if (index < 0xFE00)
+                {
+                    //internal work RAM shadow
+                    wram[index - 0xE000] = value;
+                }
+                else if (index < 0xFEA0)
+                {
+                    //sprite OAM (160 bytes)
+                    throw new NotImplementedException($"Unsupported write to $${index:X4}: Sprite OAM is not yet implemented.");
+                }
+                else if (index < 0xFF80)
+                {
+                    //various hardware I/O registers (PPU, APU, joypad, etc)
+                    throw new NotImplementedException($"Unsupported write to address ${index:X4}: Hardware I/O registers are not yet implemented.");
+                }
+                else if (index < 0xFFFF)
+                {
+                    //zero page (128 bytes)
+                    zram[index - 0xFF80] = value;
+                }
+                else if (index == 0xFFFF)
+                {
+                    interruptEnableFlag = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException($"Unexpected write to address ${index:X4}");
+                }
+            }
+        }
+
+        public IEnumerator<byte> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
