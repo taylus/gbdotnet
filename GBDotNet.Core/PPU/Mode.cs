6586ed18ad5debe406d98ba616d90660b4a079d0
taylus <brandon@legacy.codes>
2020-01-16 21:49:43 -0600

    Initial PPU branch WIP

---
+++ GBDotNet.Core/PPU/Mode.cs
@@ -0,0 +1,13 @@
+﻿namespace GBDotNet.Core
+{
+    public partial class PPU
+    {
+        private enum Mode
+        {
+            HBlank = 0,
+            VBlank = 1,
+            OamScan = 2,
+            HDraw = 3
+        }
+    }
+}
---
+++ GBDotNet.Core/PPU/PPU.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GBDotNet.Core
+{
+    /// <summary>
+    /// Implement's the Game Boy's Picture Processing Unit, which produces video
+    /// based off data in VRAM, OAM, and various memory-mapped I/O registers. 
+    /// </summary>
+    /// <remarks>
+    /// The PPU is a state machine which spends a very specific number of cycles
+    /// in each state. Timing w/ the CPU is implemented by counting cycles.
+    /// </remarks>
+    /// <see cref="https://mgba-emu.github.io/gbdoc/#ppu"/>
+    public partial class PPU
+    {
+        private Mode currentMode;
+        private int currentLineBeingDrawn;
+        private int cyclesSinceLastModeChange;
+
+        public Registers Registers { get; private set; }
+        public IMemory Memory { get; private set; }
+
+        public PPU(IMemory memory)
+        {
+            Memory = memory;
+        }
+
+        public void Tick(int elapsedCycles)
+        {
+            cyclesSinceLastModeChange += elapsedCycles;
+
+            //TODO: implement per:
+            //http://bgb.bircd.org/pandocs.htm#lcdstatusregister
+            //http://imrannazar.com/GameBoy-Emulation-in-JavaScript:-GPU-Timings
+
+            if (currentMode == Mode.HBlank)
+            {
+
+            }
+            else if (currentMode == Mode.VBlank)
+            {
+
+            }
+            else if (currentMode == Mode.OamScan)
+            {
+
+            }
+            else if (currentMode == Mode.HDraw)
+            {
+
+            }
+        }
+    }
+}
