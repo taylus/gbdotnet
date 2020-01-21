3ab854f503a24e74f19f66dac2576ed2a947ca9c
taylus <brandon@legacy.codes>
2020-01-20 22:49:47 -0600

    FIx file paths in tests to work cross-platform

--- GBDotNet.Core.Test/PPU/PPU_Should.cs
+++ GBDotNet.Core.Test/PPU/PPU_Should.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GBDotNet.Core.Test
 {
@@ -8,11 +9,11 @@ namespace GBDotNet.Core.Test
         [TestMethod]
         public void Generate_Expected_Tileset_Pixels_From_Known_VRAM_Dump()
         {
-            var vram = Memory.FromFile(@"PPU\Input\tetris.tileset.dump");
+            var vram = Memory.FromFile(Path.Combine("PPU", "Input", "tetris.tileset.dump"));
             var ppu = new PPU(new PPURegisters(), vram);
 
             var actualPixels = ppu.RenderTileSet();
-            var expectedPixels = ImageHelper.LoadImageAsPaletteIndexedByteArray(@"PPU\Expected\tetris.tileset.png");
+            var expectedPixels = ImageHelper.LoadImageAsPaletteIndexedByteArray(Path.Combine("PPU", "Expected", "tetris.tileset.png"));
 
             CollectionAssert.AreEqual(expectedPixels, actualPixels, "Rendered tileset does not match expected image.");
         }
