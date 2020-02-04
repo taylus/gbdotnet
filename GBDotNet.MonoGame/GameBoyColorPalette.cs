bb2edd289e3691ccc2326b7c0f6446927765dc05
Brandon Johnson <brandon@legacy.codes>
2020-02-04 11:06:14 -0600

    Copy MonoGameBoy into main solution (#23)

---
+++ GBDotNet.MonoGame/GameBoyColorPalette.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace MonoGameBoy
+{
+    public class GameBoyColorPalette
+    {
+        public IList<Color> Colors { get; private set; }
+
+        public GameBoyColorPalette(params Color[] colors)
+        {
+            Colors = colors;
+        }
+
+        public Color this[int i]
+        {
+            get { return Colors[i]; }
+            set { Colors[i] = value; }
+        }
+
+        /// <summary>
+        /// The original Game Boy (aka DMG for "Dot Matrix Game") greenscale color palette.
+        /// </summary>
+        public static GameBoyColorPalette Dmg => new GameBoyColorPalette(new Color(224, 248, 208),
+            new Color(136, 192, 112), new Color(52, 104, 86), new Color(8, 24, 32));
+
+        /// <summary>
+        /// The Game Boy Pocket grayscale color palette.
+        /// </summary>
+        public static GameBoyColorPalette Pocket = new GameBoyColorPalette(new Color(232, 232, 232),
+            new Color(160, 160, 160), new Color(88, 88, 88), new Color(16, 16, 16));
+    }
+}
