50c91e16f1c548f79a322c49b4e6613ed870e4c6
taylus <brandon@legacy.codes>
2020-03-14 20:03:29 -0500

    Remove things that aren't used yet

--- GBDotNet.Core/Audio/APU.cs
+++ GBDotNet.Core/Audio/APU.cs
@@ -6,16 +6,8 @@ namespace GBDotNet.Core
     public class APU
     {
         private const int samplesToGenerateBeforePlaying = 100;
-        private readonly PulseChannel channel1 = new PulseChannel(samplesToGenerateBeforePlaying);
         private readonly PulseChannel channel2 = new PulseChannel(samplesToGenerateBeforePlaying);
-        //TODO: wave channel 3, noise channel 4
-
         private readonly IList<float> mixedBuffer = new List<float>(samplesToGenerateBeforePlaying);
-        //TODO: global sound control registers http://bgb.bircd.org/pandocs.htm#soundcontrolregisters
-
-        private int cyclesSinceLastFrameSequencerStep;
-        private const int cyclesPerFrameSequencerStep = 8192;   //512 Hz (4194304 Hz / 512 Hz = 8192 cycles)
-        private int frameSequencerStep;                         //0-7, different things update on different steps
 
         private const int sampleRate = 44100;   //Hz
         private int cyclesSinceLastSample;
@@ -30,42 +22,8 @@ namespace GBDotNet.Core
 
         public void Tick(int elapsedCycles)
         {
-            //channel1.Tick();
             channel2.Tick();
-
-            TickFrameSequencer(elapsedCycles);
             SampleChannels(elapsedCycles);
-        }
-
-        private void TickFrameSequencer(int elapsedCycles)
-        {
-            cyclesSinceLastFrameSequencerStep += elapsedCycles;
-            if (cyclesSinceLastFrameSequencerStep >= cyclesPerFrameSequencerStep)
-            {
-                cyclesSinceLastFrameSequencerStep = 0;
-                StepFrameSequencer();
-            }
-        }
-
-        private void StepFrameSequencer()
-        {
-            if (frameSequencerStep % 2 == 0)
-            {
-                //TODO: tick length counter every other step
-            }
-
-            if (frameSequencerStep == 7)
-            {
-                //TODO: tick volume envelope every 7th step
-            }
-
-            if (frameSequencerStep == 2 || frameSequencerStep == 6)
-            {
-                //TODO: tick sweep every 2nd and 6th steps
-            }
-
-            frameSequencerStep++;
-            if (frameSequencerStep > 7) frameSequencerStep = 0;
         }
 
         private void SampleChannels(int elapsedCycles)
@@ -75,7 +33,6 @@ namespace GBDotNet.Core
             {
                 cyclesSinceLastSample = 0;
                 float sample = 0;
-                //sample += channel1.Sample();
                 sample += channel2.Sample();
                 mixedBuffer.Add(sample);
             }
